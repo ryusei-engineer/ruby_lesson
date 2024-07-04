f8336c84f258ac2a28a1e906ff1c8980424cf5b9
ryusei-engineer <ryusei.engineer@gmail.com>
2024-07-04 23:46:24 +0900

    文字列とto.i to.f リテラル

--- index.rb
+++ index.rb
@@ -1,2 +1,17 @@
-puts "Hello, world!"
-puts 1 + 2
+puts 'hello world'
+# コメント
+s = 'hello'
+
+puts s 
+
+TAX = 1.08
+puts TAX
+
+first_name = 'tanaka'
+last_name = 'taro'
+
+puts "My name is #{first_name} #{last_name}"
+
+puts 1 + '1'.to_i
+
+puts 1 + '1'.to_f
